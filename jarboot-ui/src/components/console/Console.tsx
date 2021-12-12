84b707e30f728db7c4cb6bf75b0000eb97c83bcb
majianzheng <282295811@qq.com>
2021-12-12 16:46:12 +0800

    refactor: 更新Console初始界面显示画面

--- jarboot-ui/src/components/console/Console.tsx
+++ jarboot-ui/src/components/console/Console.tsx
@@ -378,7 +378,58 @@ class Console extends React.PureComponent<ConsoleProps> {
             style.height = this.props.height;
         }
         return <code id={`id-console-${this.props.id}`} style={style} className={styles.console}>
-            <p className={styles.consoleTitle}>Jarboot Console</p>
+            <div className={styles.banner}>
+                <br/>
+                <p>
+                    <span className={styles.red}><span>&nbsp;&nbsp; &nbsp; </span>,--.</span>
+                    <span className={styles.green}><span>&nbsp; &nbsp; &nbsp; &nbsp; </span></span>
+                    <span className={styles.yellow}> <span>&nbsp; &nbsp; &nbsp; </span></span>
+                    <span className={styles.blue}>,--. <span>&nbsp; </span></span>
+                    <span className={styles.magenta}><span>&nbsp; &nbsp; &nbsp; &nbsp;</span></span>
+                    <span className={styles.cyan}> <span>&nbsp; &nbsp; &nbsp; </span></span>
+                    <span className={styles.red}><span>&nbsp; </span>,--.<span>&nbsp; &nbsp;</span></span>
+                </p>
+                <p>
+                    <span className={styles.red}><span>&nbsp;&nbsp; &nbsp; </span>|<span>&nbsp; </span>|</span>
+                    <span className={styles.green}> ,--,--.</span>
+                    <span className={styles.yellow}>,--.--.</span>
+                    <span className={styles.blue}>|<span>&nbsp; </span>|-. </span>
+                    <span className={styles.magenta}> ,---. </span>
+                    <span className={styles.cyan}> ,---. </span>
+                    <span className={styles.red}>,-'<span>&nbsp; </span>'-. </span>
+                </p>
+                <p>
+                    <span className={styles.red}>,--. |<span>&nbsp; </span>|</span>
+                    <span className={styles.green}>' ,-.<span>&nbsp; </span>|</span>
+                    <span className={styles.yellow}>|<span>&nbsp; </span>.--'</span>
+                    <span className={styles.blue}>| .-. '</span>
+                    <span className={styles.magenta}>| .-. |</span>
+                    <span className={styles.cyan}>| .-. |</span>
+                    <span className={styles.red}>'-.<span>&nbsp; </span>.-' </span>
+                </p>
+                <p>
+                    <span className={styles.red}>|<span>&nbsp; </span>'-'<span>&nbsp; </span>/</span>
+                    <span className={styles.green}>\ '-'<span>&nbsp; </span>|</span>
+                    <span className={styles.yellow}>|<span>&nbsp; </span>| </span>
+                    <span className={styles.blue}>  | `-' |</span>
+                    <span className={styles.magenta}>' '-' '</span>
+                    <span className={styles.cyan}>' '-' '</span>
+                    <span className={styles.red}><span>&nbsp; </span>|<span>&nbsp; </span>|<span>&nbsp; &nbsp;</span></span>
+                </p>
+                <p>
+                    <span className={styles.red}> `-----' </span>
+                    <span className={styles.green}> `--`--'</span>
+                    <span className={styles.yellow}>`--'<span>&nbsp; &nbsp;</span></span>
+                    <span className={styles.blue}> `---' </span>
+                    <span className={styles.magenta}> `---' </span>
+                    <span className={styles.cyan}> `---' </span>
+                    <span className={styles.red}><span>&nbsp; </span>`--'<span>&nbsp; &nbsp;</span></span>
+                </p>
+                <br/>
+                <br/>
+                <p>Jarboot console, docs: <span className={styles.cyan}>{JarBootConst.DOCS_URL}</span></p>
+                <p>Diagnose command, type ‘help’ and hit ‘ENTER’ to see.</p>
+            </div>
         </code>;
     }
 }
