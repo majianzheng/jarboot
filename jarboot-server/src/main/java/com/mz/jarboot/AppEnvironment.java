fb8b5f39dbfaa03ad6822181e90562f6f955e118
majianzheng <282295811@qq.com>
2021-12-13 18:12:54 +0800

    refactor: 环境检查代码优化

--- jarboot-server/src/main/java/com/mz/jarboot/AppEnvironment.java
+++ jarboot-server/src/main/java/com/mz/jarboot/AppEnvironment.java
@@ -1,11 +1,13 @@
 package com.mz.jarboot;
 
 import com.mz.jarboot.api.constant.CommonConst;
+import com.mz.jarboot.api.exception.JarbootRunException;
+import com.mz.jarboot.common.AnsiLog;
 import com.mz.jarboot.common.CacheDirHelper;
+import com.mz.jarboot.common.VMUtils;
 import com.mz.jarboot.common.VersionUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.swing.*;
 import java.io.File;
 
 /**
@@ -25,79 +27,69 @@ public class AppEnvironment {
                 homePath = System.getProperty("user.home") + File.separator + CommonConst.JARBOOT_NAME;
             }
         }
-        if (homePath.contains(StringUtils.SPACE)) {
-            JOptionPane.showConfirmDialog(
-                    null,
-                    "Jarboot路径中存在空格！" ,
-                    "错误",
-                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+        //检查安装路径是否存在空格
+        if (StringUtils.containsWhitespace(homePath)) {
+            AnsiLog.error("Jarboot所在目录的全路径中存在空格！");
+            System.exit(-1);
         }
-        System.setProperty(CommonConst.JARBOOT_HOME, homePath);
+
+        if (null == System.getProperty(CommonConst.JARBOOT_HOME, null)) {
+            System.setProperty(CommonConst.JARBOOT_HOME, homePath);
+        }
         System.setProperty("application.version", "v" + VersionUtils.version);
+        //derby数据库驱动的日志文件位置
         final String derbyLog = homePath + File.separator + "logs" + File.separator + "derby.log";
         System.setProperty("derby.stream.error.file", derbyLog);
 
         //环境检查
-        if (!checkEnvironment()) {
+        try {
+            checkEnvironment();
+            //初始化cache目录
+            CacheDirHelper.init();
+        } catch (Exception e) {
+            AnsiLog.error(e);
             System.exit(-1);
         }
-        //初始化cache目录
-        CacheDirHelper.init();
     }
 
-    private static void propDialog(String msg) {
-        JOptionPane.showConfirmDialog(null, msg , "错误",
-                JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
-    }
-
-    private static boolean checkEnvironment() {
+    private static void checkEnvironment() {
         String dir;
         //先检查jarboot-agent.jar文件
         File jarFile;
         String binDir = System.getProperty(CommonConst.JARBOOT_HOME) + File.separator + "bin";
         try {
-
             jarFile = new File(binDir, CommonConst.AGENT_JAR_NAME);
             //先尝试从当前路径下获取jar的位置，若不存在则尝试从用户目录加载
             if (!jarFile.exists()) {
-                propDialog("检查环境错误，在当前目录未发现jarboot-agent.jar。");
-                return false;
+                throw new JarbootRunException("检查环境错误，在当前目录未发现jarboot-agent.jar。");
             }
             dir = jarFile.getParent();
         } catch (Exception e) {
             //查找jarboot-agent.jar失败
-            propDialog("检查环境错误，缺失jarboot-agent.jar，请确保文件的完整性。错误：" + e.getMessage());
-            return false;
+            throw new JarbootRunException("检查环境错误，缺失jarboot-agent.jar，请确保文件的完整性。错误：" + e.getMessage());
         }
         //检查jarboot-core.jar文件，该文件必须和jarboot-agent.jar处于同一目录下
         jarFile = new File(dir, "jarboot-core.jar");
         if (!jarFile.exists()) {
-            propDialog("检查环境错误，未发现jarboot-core.jar。");
-            return false;
+            throw new JarbootRunException("检查环境错误，未发现jarboot-core.jar。");
         }
         if (!jarFile.isFile()) {
-            propDialog("检查环境错误，jarboot-core.jar不是文件类型。");
-            return false;
+            throw new JarbootRunException("检查环境错误，jarboot-core.jar不是文件类型。");
         }
 
         jarFile = new File(dir, "jarboot-spy.jar");
         if (!jarFile.exists()) {
-            propDialog("检查环境错误，未发现jarboot-spy.jar。");
-            return false;
+            throw new JarbootRunException("检查环境错误，未发现jarboot-spy.jar。");
         }
         if (!jarFile.isFile()) {
-            propDialog("检查环境错误，jarboot-spy.jar不是文件类型。");
-            return false;
+            throw new JarbootRunException("检查环境错误，jarboot-spy.jar不是文件类型。");
         }
 
         //检查是否是jdk环境，是否存在tools.jar
-        try {
-            Class.forName("com.sun.tools.attach.VirtualMachine");
-        } catch (ClassNotFoundException e) {
-            propDialog("检查环境错误，当前运行环境未安装jdk，请检查环境变量是否配置，可能是使用了jre环境。");
-            return false;
+        if (VMUtils.getInstance().check()) {
+            return;
         }
-        return true;
+        throw new JarbootRunException("检查环境错误，当前运行环境未安装jdk，请检查环境变量是否配置，可能是使用了jre环境。");
     }
 
     private AppEnvironment() { }
