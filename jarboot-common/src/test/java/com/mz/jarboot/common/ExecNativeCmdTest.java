09e8303b4a1791616575b18405b232805a810bf6
majianzheng <282295811@qq.com>
2021-12-26 18:17:32 +0800

    feature: 更新单元测试

--- jarboot-common/src/test/java/com/mz/jarboot/common/ExecNativeCmdTest.java
+++ jarboot-common/src/test/java/com/mz/jarboot/common/ExecNativeCmdTest.java
@@ -1,5 +1,7 @@
 package com.mz.jarboot.common;
 
+import com.mz.jarboot.common.utils.OSUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,14 +13,26 @@ import static org.junit.Assert.assertFalse;
  * @author majianzheng
  */
 public class ExecNativeCmdTest {
+    private static String cmd = "";
+    private static String[] cmdArray;
+    @BeforeClass
+    public static void beforeTest() {
+        if (OSUtils.isWindows()) {
+            cmd = "cmd /c ";
+            cmdArray = new String[]{"cmd", "/c", "echo", "Hi"};
+        } else {
+            cmdArray = new String[]{"echo", "Hi"};
+        }
+        cmd += "echo Hello";
+    }
 
     @Test
     public void testExec() {
-        List<String> result = ExecNativeCmd.exec("echo Hello");
+        List<String> result = ExecNativeCmd.exec(cmd);
         assertFalse(result.isEmpty());
         assertEquals(1, result.size());
         assertEquals("Hello", result.get(0));
-        result = ExecNativeCmd.exec(new String[]{"echo", "Hi"});
+        result = ExecNativeCmd.exec(cmdArray);
         assertFalse(result.isEmpty());
         assertEquals(1, result.size());
         assertEquals("Hi", result.get(0));
@@ -26,14 +40,14 @@ public class ExecNativeCmdTest {
 
     @Test
     public void testGetFirstAnswer() {
-        String result = ExecNativeCmd.getFirstAnswer("echo Hello");
+        String result = ExecNativeCmd.getFirstAnswer(cmd);
         assertFalse(result.isEmpty());
         assertEquals("Hello", result);
     }
 
     @Test
     public void testGetAnswerAt() {
-        String result = ExecNativeCmd.getAnswerAt("echo Hello", 0);
+        String result = ExecNativeCmd.getAnswerAt(cmd, 0);
         assertFalse(result.isEmpty());
         assertEquals("Hello", result);
     }
